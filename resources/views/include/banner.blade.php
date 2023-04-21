d055eb1a92ff63c1af1f0a863235f4402a20cad7
812111010 <812111010@utchetumal.edu.mx>
2023-04-21 16:42:29 -0500

    uno

---
+++ resources/views/include/banner.blade.php
@@ -0,0 +1,12 @@
+<head>
+@vite(['resources/css/include/banner.css'])
+
+</head>
+
+<div class="pre-banner">
+    <div class="banner">
+        <main class="banner-main">
+            <a href="/"><img src="" alt=""></a>
+        </main>
+    </div>
+</div>
