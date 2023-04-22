133cc422af70f75e6e541e0a2467567f424fcfc0
812111010 <812111010@utchetumal.edu.mx>
2023-04-21 22:40:38 -0500

    pruebaVite10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,6 @@
         </div>
         <div class="welcome-two">
             <p>{{ __('Social media')}}</p>
-            <img class="welcome-img" src="{{ Vite::asset('resources/img/login/modelo-login.png') }}">
             <a href="https://www.facebook.com/UTChetumal/" class="facebook" target="_blank"><i class="fa-brands fa-facebook-f facebook-icon"></i></a>
             <a href="https://www.instagram.com/ut_chetumal/" class="instagram" target="_blank"><i class="fa-brands fa-instagram instagram-icon"></i></a>
             <a href="https://twitter.com/UTChetumal" class="twitter" target="_blank"><i class="fa-brands fa-twitter twitter-icon"></i></a>
