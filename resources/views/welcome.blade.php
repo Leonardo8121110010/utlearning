609aa0464d40e181f2a89085469670e9c3b25550
812111010 <812111010@utchetumal.edu.mx>
2023-04-21 23:31:42 -0500

    sAssete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
         </div>
         <div class="welcome-two">
             <p>{{ __('Social media')}}</p>
-            <img class="welcome-img" src="{{ asset('img/login/modelo-login.png') }}">
+            <img class="welcome-img" src="{{ secure_asset('img/login/modelo-login.png') }}">
             <a href="https://www.facebook.com/UTChetumal/" class="facebook" target="_blank"><i class="fa-brands fa-facebook-f facebook-icon"></i></a>
             <a href="https://www.instagram.com/ut_chetumal/" class="instagram" target="_blank"><i class="fa-brands fa-instagram instagram-icon"></i></a>
             <a href="https://twitter.com/UTChetumal" class="twitter" target="_blank"><i class="fa-brands fa-twitter twitter-icon"></i></a>
