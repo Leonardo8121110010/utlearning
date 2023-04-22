682da2fb777545d26442cd87a1cb9d8412c372d5
812111010 <812111010@utchetumal.edu.mx>
2023-04-21 22:39:02 -0500

    pruebaVite9

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -3,6 +3,7 @@ import '../css/app.css';
 import '../css/include/banner.css';
 import '../css/include/announcements.css';
 import '../css/include/numbers.css';
+import '../img/login/modelo-login.png';
 
 
 
